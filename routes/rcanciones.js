f0fce80d5362e4fed9920b0686a0bd0934e700b8
UO270930 <marcostobias64@gmail.com>
2021-03-25 18:05:31 +0100

    SDI-2021-314-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,12 +1,20 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
+        let canciones = [ {
+            "nombre": "Blank space",
+            "precio": "1.2"
+        }, {
+            "nombre": "See you again",
+            "precio": "1.3"
+        }, {
+            "nombre": "Uptown Funk",
+            "precio": "1.1"
+        }];
 
-        if(req.query.nombre != null)
-            respuesta += "Nombre: " + req.query.nombre + "<br>";
-
-        if(typeof (req.query.autor) != "undefined")
-                respuesta += "Autor: " + req.query.autor;
+        let respuesta = swig.renderFile("views/btienda.html", {
+            vendedor: "Tienda de canciones",
+            canciones: canciones
+        });
 
         res.send(respuesta);
     });
@@ -15,6 +23,14 @@ module.exports = function(app) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
 
         res.send(String(respuesta));
+    });
+
+    app.get("/canciones/agregar", function(req, res) {
+        let respuesta = swig.renderFile("views/bagregar.html", {
+
+        });
+
+        res.send(respuesta);
     });
 
     app.get("/canciones/:id", function(req, res) {
@@ -37,5 +53,5 @@ module.exports = function(app) {
 
     app.get("/promo", function(req, res) {
         res.send("Respuesta patrón promo* ");
-    })
+    });
 };
