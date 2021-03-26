2d29c2ebc09e7cc26e6eb60d6f12e530a9dcfd5b
UO270930 <marcostobias64@gmail.com>
2021-03-26 10:16:21 +0100

    SDI-2021-314-sesión7-Ejercicio-complementario3

--- routes/rautores.js
+++ routes/rautores.js
@@ -49,18 +49,48 @@ module.exports = function(app, swig) {
         res.send(respuesta);
     });
 
+    app.get("/autores/filtrar/:rol", function(req, res) {
+        let autores = [{
+            nombre: "CantanteGenerico",
+            grupo: "grupoGenerico1",
+            rol: "Cantante"
+        }, {
+            nombre: "BateríaGenerico",
+            grupo: "grupoGenerico2",
+            rol: "Batería"
+        }, {
+            nombre: "GuitarristaGenerico",
+            grupo: "grupoGenerico3",
+            rol: "Guitarrista"
+        }, {
+            nombre: "BajistaGenerico",
+            grupo: "grupoGenerico4",
+            rol: "Bajista"
+        }, {
+            nombre: "TeclistaGenerico",
+            grupo: "grupoGenerico5",
+            rol: "Teclista"
+        }];
+
+        autores = autores.filter(autor => autor.rol.toLowerCase()  === req.params.rol.toLowerCase());
+
+        let respuesta = swig.renderFile("views/autores.html", {
+            autores: autores
+        });
+
+        res.send(respuesta);
+    });
+
     app.get("/autores*", function(req, res) {
         res.redirect("/autores");
     });
 
     app.post("/autor", function(req, res) {
-        let nombre = req.body.nombre == "" ? "Nombre no enviado en la petición" : req.body.nombre;
-        let grupo = req.body.grupo == "" ? "Grupo no enviado en la petición" : req.body.grupo;
-        let rol = req.body.rol == "" ? "Rol no enviado en la petición" : req.body.rol;
+        let nombre = req.body.nombre === "" ? "Nombre no enviado en la petición" : req.body.nombre;
+        let grupo = req.body.grupo === "" ? "Grupo no enviado en la petición" : req.body.grupo;
+        let rol = req.body.rol === "" ? "Rol no enviado en la petición" : req.body.rol;
 
         res.send("Autor agregado: " + "<br>" + "Nombre: " + nombre + "<br>"
                         + "Grupo: " + grupo + "<br>" + "Rol: " + rol);
     });
-
-    app.get("")
 };
