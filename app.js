0b668ed37a9b0141701cba3ce0ca3e89ebd7f985
UO270930 <marcostobias64@gmail.com>
2021-04-16 11:31:53 +0200

    SDI-2021-314-10.1-SW API REST CRUD Canciones.

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rcomentarios.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig);
 require("./routes/rfavoritos.js")(app, swig, gestorBD);
+require("./routes/rapicanciones.js")(app, gestorBD);
 
 app.get("/", function(req, res) {
     res.redirect("/tienda");
